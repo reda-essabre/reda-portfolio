a8847731c820ade1763be9790625a55f8c5df870
Reda Essabre <re.essabre@gmail.com>
2026-03-14 01:59:10 +0100

    Rollback main to change 39 state

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,3 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+export function cn(...inputs: Array<string | false | null | undefined>) {
+  return inputs.filter(Boolean).join(" ")
 }
